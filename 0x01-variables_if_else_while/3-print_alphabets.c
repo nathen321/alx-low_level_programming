7b49a6bb6c1f5f0c09411753ff020c6eec3d3516
nathen321 <walidjalaly43@gmail.com>
2023-10-06 17:21:33 -0700

    ok

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints from A to Z.
+ * Return: Always 0.
+*/
+int main(void)
+{
+	char alph;
+
+	char alphcap;
+
+	for (alph = 97; alph <= 122; alph++)
+		putchar(alph);
+	putchar('\n');
+	for (alphcap = 'A'; alphcap <= 'Z'; alphcap++)
+		putchar(alphcap);
+	putchar('\n');
+
+	return (0);
+}
