fff0dac25b1ec8089a2b07c9aedd2918acd443e8
nathen321 <walidjalaly43@gmail.com>
2023-10-06 17:08:02 -0700

    ok

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints from A to Z.
+ * Return: Always 0.
+*/
+int main(void)
+{
+	char alph = 97;
+	for (alph = 97; alph <= 122; alph++)
+		putchar(alph);
+	putchar('\n');
+
+	return (0);
+}
