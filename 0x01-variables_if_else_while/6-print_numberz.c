0f7dc0588636e70790989f21fa41fbf7cb2c93e6
nathen321 <walidjalaly43@gmail.com>
2023-10-06 18:26:41 -0700

    ok

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints from A to Z.
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+		putchar(num);
+	putchar('\n');
+
+	return (0);
+}
