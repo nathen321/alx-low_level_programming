9d37161caf514511fb16da8ee77a61cc2405211f
nathen321 <walidjalaly43@gmail.com>
2023-10-12 09:48:50 -0700

    ok

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,11 @@
 */
 void print_numbers(void)
 {
-int number = 48;
-for (number = 48; number < 58; number++)
-{
-_putchar(number);
-}
-_putchar(10);
+	int number = 48;
+
+	for (number = 48; number < 58; number++)
+	{
+		_putchar(number);
+	}
+	_putchar(10);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+
+
+
+
+#endif
