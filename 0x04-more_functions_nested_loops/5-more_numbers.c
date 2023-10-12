b132e6208ba18cd21e9da66912aa51c3f74c9d1e
nathen321 <walidjalaly43@gmail.com>
2023-10-12 11:40:29 -0700

    ok

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - function that print all numbers from
+ *0 to 9 ten time
+ *Return: returns nothing
+*/
+void more_numbers(void);
+{
+	int num = 48;
+	for (i = 0; i <= 10; i++)
+	{
+		for (num = 48; num < 58; num++)
+		{
+			_putchar(num);
+		}
+		_putchar('\n');
+	}
+}
