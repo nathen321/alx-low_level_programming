db2677af9285a1d29318db6e75221780c72d17e5
nathen321 <walidjalaly43@gmail.com>
2023-10-12 11:42:02 -0700

    ok

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,9 +4,11 @@
  *0 to 9 ten time
  *Return: returns nothing
 */
-void more_numbers(void);
+void more_numbers(void)
 {
 	int num = 48;
+	int i;
+
 	for (i = 0; i <= 10; i++)
 	{
 		for (num = 48; num < 58; num++)
