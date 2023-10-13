c210c6da07e5bcdb1b14a7d616f3bfdadce5449f
nathen321 <walidjalaly43@gmail.com>
2023-10-12 18:15:28 -0700

    ok

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,13 +13,13 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
-		else if ((i % 3) == 0 && (i % 5) != 0)
+		else if ((i % 3) == 0)
 		{
 			printf("Fizz ");
 		}
 		else if ((i % 5) == 0)
 		{
-			 printf("Buzz ");
+			printf("Buzz ");
 		}
 		else
 		{
