c3bb41cf04d013eda0421a23d76dd86d71ab43e7
nathen321 <walidjalaly43@gmail.com>
2023-10-17 15:27:33 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * puts_half -  prints two half of the string
+ * @str: the string to be halfed
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int x, y = 0;
+
+	while (str[y] != '\0')
+	{
+		y++;
+	}
+	if ((y % 2) == 1)
+	{
+		x = (y - 1) / 2;
+		for (; x < y; x++)
+		{
+			_putchar(str[x]);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		x = y / 2;
+		for (; x < y; x++)
+		{
+			_putchar(str[x]);
+		}
+		_putchar('\n');
+	}
+}
