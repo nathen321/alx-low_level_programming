d2f5e0ab9e50bf589991d03fe7b62ee02205518f
nathen321 <walidjalaly43@gmail.com>
2023-10-17 13:56:11 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - revers the string str
+ * @s: the str to be reversed
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int z, y = 0;
+	char h = s[0];
+
+	while (s[y] != '\0')
+		y++;
+ 
+	for (z = 0; z < y; z++)
+	{
+		y--;
+		h = s[z];
+		s[z] = s[y];
+		s[y]  = h;
+	}
+}
