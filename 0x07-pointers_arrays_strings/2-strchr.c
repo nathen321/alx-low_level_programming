dc9df555dd8ef261f8943ca678be46a6fa7f0935
nathen321 <walidjalaly43@gmail.com>
2023-10-23 16:52:46 -0700

    ok

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,20 +7,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	int y = 0, x;
+	int x = 0;
 
-	while (s[y])
-	{
-		y++;
-	}
-
-	for (x = 0; x <= y; x++)
+	while (s[x])
 	{
 		if (c == s[x])
 		{
-			s += x;
+			s += x + 1;
 			return (s);
 		}
+		x++;
 	}
 	return ('\0');
 }
