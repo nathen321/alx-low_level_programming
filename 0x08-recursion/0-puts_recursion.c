86ed59f262a73bff7d651e361817319e33072671
nathen321 <walidjalaly43@gmail.com>
2023-10-25 14:23:12 -0700

    ok

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _puts_recursion - check the code
+ * @s: what to print
+ * Return: void.
  */
 void _puts_recursion(char *s)
 {
