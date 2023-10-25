0095025a1077e2f0128dd145dc042e211d7de0de
nathen321 <walidjalaly43@gmail.com>
2023-10-24 17:22:03 -0700

    ok

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  * *_strpbrk - search for the firdt ocurance acc in s
+  * @s: source string
+  * @accept: accepted string
+  *
+  * Return: number of bytes in the init segment
+  */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int z = 0, x;
+
+	while (s[z])
+	{
+		x = 0;
+
+		while (accept[x])
+		{
+			if (accept[z] == s[x])
+			{
+				return (s + z);
+			}
+			x++;
+		}
+
+		z++;
+	}
+
+	return ('\0');
+}
