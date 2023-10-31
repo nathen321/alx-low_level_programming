847f89698106684ed9ae9ea1d391cee156c97df0
nathen321 <walidjalaly43@gmail.com>
2023-10-31 08:21:54 -0700

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *create_arrary- create an array
+ * @c: content of th e array
+ * @size: the size of the memory to print
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *t;
+	unsigned int x;
+
+	if (size == 0)
+		return (NULL);
+	t = malloc(sizeof(t) * c);
+	for (x = 0; x <= size; x++)
+		t[x] = c;
+	return (t);
+}
