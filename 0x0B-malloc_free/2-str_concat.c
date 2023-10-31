eca26156a397f6a86144c10161c3aeef60eeef7b
nathen321 <walidjalaly43@gmail.com>
2023-10-31 16:28:31 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,20 +8,23 @@
  * @str: content of the array
  * Return: Nothing.
  */
-char *str_concat(char *s1, char *s2);
+char *str_concat(char *s1, char *s2)
 {
 	char *t;
-	unsigned int x;
+	unsigned int x, y;
 
-	if (str == NULL)
-		return (NULL);
+	if (s1 == NULL || s2 == NULL)
+		return (" ");
 	t = malloc((sizeof(char) * (strlen(s2) + strlen(s1))) + 1);
 	if (t == NULL)
 		return (NULL);
 	for (x = 0; x < strlen(s1); x++)
 		t[x] = s1[x];
-	for (x = 0; x < strlen(s2); x++)
-		t[x] = s2[x];
+	for (y = 0; y < strlen(s2); x++)
+	{
+		t[x] = s2[y];
+		y++;
+	}
 	t[x] = '\0';
 	return (t);
 }
