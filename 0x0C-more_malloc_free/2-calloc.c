8c4c25a5dff5228876684c2dc5e87551644db664
nathen321 <walidjalaly43@gmail.com>
2023-11-02 13:35:48 -0700

    malloc test

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * _calloc - prints buffer in hexa
@@ -11,17 +13,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
 	char *t;
-
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
 	t = malloc(nmemb * size);
-
 	if (t == NULL)
 		return (NULL);
-
 	for (i = 0; i < nmemb - 1; i++)
 		t[i] = 0;
-
 	return (t);
 }
