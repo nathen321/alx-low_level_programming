c6c6ae20a8109c57a98e77bf646950efd9bbf5ae
nathen321 <walidjalaly43@gmail.com>
2023-11-02 08:53:16 -0700

    malloc test

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,7 +21,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	t = malloc(sizeof(char) * (v + n));
+	t = malloc(sizeof(char) * (v + n + 1));
 	if (t == NULL)
 		return (NULL);
 	for (y = 0; y < v; y++)
