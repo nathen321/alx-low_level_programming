4d892992c7fa44f5e70921bb97c1e9db92d51aa9
nathen321 <walidjalaly43@gmail.com>
2023-11-02 13:19:36 -0700

    malloc test

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - prints buffer in hexa
+ * @nmemb: number of element in arrey
+ * @size: the size of the memory to print
+ *
+ * Return: arrey.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *t;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	t = malloc(nmemb * size);
+
+	if (t == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		t[i] = 0;
+
+	return (t);
+}
