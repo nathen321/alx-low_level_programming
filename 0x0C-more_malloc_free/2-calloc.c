27d673a3032c7a4c3d70067c71b584368c7d6183
nathen321 <walidjalaly43@gmail.com>
2023-11-02 13:32:37 -0700

    malloc test

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (t == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb - 1; i++)
 		t[i] = 0;
 
 	return (t);
