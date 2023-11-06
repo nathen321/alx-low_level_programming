f8392dd8ce2bad877791fc49e1c5cdbef9cac9e1
nathen321 <walidjalaly43@gmail.com>
2023-11-06 12:33:25 -0800

    cpp

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION
-#define FUNCTION
-#define ABS(a) a < 0 ? ((a) * -1) : (x)
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+#define ABS(a) ((a) < 0 ? ((a) * -1) : (a))
 
 #endif
