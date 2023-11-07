455301b898d8d31fc83313d47fd0f70f046e7712
nathen321 <walidjalaly43@gmail.com>
2023-11-06 17:07:58 -0800

    typedef test

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,16 @@
 #ifndef DOG
 #define DOG
-struct dog {
+/**
+  * struct dog - Dog attributes
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The owner of the dog.
+  *
+  * Description: The attributes of a dog.
+  */
+typedef struct dog {
 	char *name;
 	float age;
 	char *owner;
-}
+} dog;
 #endif
