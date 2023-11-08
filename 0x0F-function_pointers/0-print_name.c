1065e29566f04b2835e7096c7a1667bea5ab587f
nathen321 <walidjalaly43@gmail.com>
2023-11-08 14:14:58 -0800

    function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include "function_pointers.h"
 
 /**
  * print_name - prints a name as is
  * @name: name of the person
- *
+ * @f: function use
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
