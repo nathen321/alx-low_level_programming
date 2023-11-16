bcc74d58b65eb183a03710ae5852b26615588729
nathen321 <walidjalaly43@gmail.com>
2023-11-16 15:31:41 -0800

    linked list corection

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - check the code
+ * @head: ....
+ * @str: ...
+ *
+ * Return: Always.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *temp;
+
+	if (str != NULL)
+	{
+		new = malloc(sizeof(list_t));
+		if (new == NULL)
+			return (NULL);
+		new->str = strdup(str);
+		new->len = strlen(str);
+		new->next = NULL;
+	}
+	if (*head == NULL)
+	{
+		*head  = new;
+		return (*head);
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next)
+			temp = temp->next;
+
+
+		temp->next = new;
+	
+		return (temp);
+	}
+
+	return (null);
+}
