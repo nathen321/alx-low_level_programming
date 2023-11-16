751377033f109f2b730912c5d11b85ef70ff13c7
nathen321 <walidjalaly43@gmail.com>
2023-11-16 14:34:30 -0800

    linked list firt commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_t - check the code
+ * @h: ....
+ *
+ * Return: Always count.
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
