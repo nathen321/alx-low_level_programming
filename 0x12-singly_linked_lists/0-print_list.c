d7fa2dc11aed6447c8e2f50b49f5ee1ac06897cc
nathen321 <walidjalaly43@gmail.com>
2023-11-16 14:22:56 -0800

    linked list firt commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - check the code
+ * @h: ....
+ *
+ * Return: Always count.
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			return (1);
+		}
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
