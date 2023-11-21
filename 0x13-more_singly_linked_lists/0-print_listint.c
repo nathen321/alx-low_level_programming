cf329865f1598060e96986767538f1f5e29204e9
nathen321 <walidjalaly43@gmail.com>
2023-11-21 03:28:19 -0800

    testing linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,11 +12,14 @@ size_t print_listint(const listint_t *h)
 {
 	unsigned int count = 0;
 
-	while (h)
+	if (h != NULL)
 	{
-		count++;
-		printf("%d\n", h->n);
-		h = h->next;
+		while (h)
+		{
+			count++;
+			printf("%d\n", h->n);
+			h = h->next;
+		}
 	}
 	return (count);
 }
