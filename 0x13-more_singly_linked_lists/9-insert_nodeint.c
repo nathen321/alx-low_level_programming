eaf8277f7a8340077bfde72e7b497277d0c2157d
nathen321 <walidjalaly43@gmail.com>
2023-11-22 02:26:28 -0800

    testing linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp, *new;
-	unsigned int i = 0;
+	unsigned int i = 1;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 	temp = *head;
-	while (i < (idx - 1))
+	while (i < idx)
 	{
 		if (temp)
 		{
