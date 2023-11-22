6ba0592974d5c4359e0bf23677835804afe81c57
nathen321 <walidjalaly43@gmail.com>
2023-11-22 02:22:17 -0800

    testing linked list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - check the code
+ * @head: ...
+ * @index: ....
+ * Return: Always 0.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp, *new;
+	unsigned int i = 0;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	temp = *head;
+	while (i < (idx - 1))
+	{
+		if (temp)
+		{
+			temp = temp->next;
+			i++;
+		}
+		else
+		{
+		return (NULL);
+		}
+	}
+	new->next = temp->next;
+	temp->next = new;
+	return (new);
+}
