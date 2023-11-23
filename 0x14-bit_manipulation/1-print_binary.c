33d7f597d432ed6b5bd5e8d8c08241886c7890d7
nathen321 <walidjalaly43@gmail.com>
2023-11-23 15:45:07 -0800

    testing

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,17 +9,39 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 0)
+	/**
+  * print_binary - Prints the binary representation of a number
+  * @n: The number to representing in binary
+  *
+  * Return: Nothing
+  */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
 	{
-		while (n)
-		{
-			if (n & 1)
-				_putchar('1');
-			else
-				_putchar('0');
-			n = n >> 1;
-		}
+		_putchar('0');
+		return;
 	}
+
+	_divide(n);
+}
+
+/**
+  * _divide - ...
+  * @n: ...
+  *
+  * Return: ...
+  */
+void _divide(unsigned long int n)
+{
+	if (n < 1)
+		return;
+
+	_divide(n >> 1);
+
+	if (n & 1)
+		_putchar('1');
 	else
 		_putchar('0');
+}
 }
