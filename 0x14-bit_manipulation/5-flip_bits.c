3fb91d724ec4d549ef145670b826e5bbaa3935e5
nathen321 <walidjalaly43@gmail.com>
2023-11-24 03:30:10 -0800

    testing

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int temp = 0, count = 0;
+
+	temp = n ^ m;
+	while (temp)
+	{
+		if (temp & 1)
+			count++;
+		temp >>= 1;
+	}
+	return (count);
+}
