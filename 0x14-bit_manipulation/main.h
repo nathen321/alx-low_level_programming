f8d2e315f5842accdf2c0db355e3b62ae1dc4006
nathen321 <walidjalaly43@gmail.com>
2023-11-24 02:07:00 -0800

    testing

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,4 +5,5 @@ void print_binary(unsigned long int n);
 int _putchar(char c);
 void _divide(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 #endif
