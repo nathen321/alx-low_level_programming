7eb19d9ee6fb64096dc44d7d14d3f85b8b531774
nathen321 <walidjalaly43@gmail.com>
2023-11-24 01:29:48 -0800

    testing

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include "main.h"
+/**
+  * get_bit - ...
+  * @n: ...
+  * @index: ...
+  * Return: ...
+  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return (getbit(n, index));
+	if (n)
+		return (getbit(n, index));
 	return (-1);
 }
 /**
