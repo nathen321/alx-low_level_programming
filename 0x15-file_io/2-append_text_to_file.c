38230491e515fb184141f429e1bdfc23cb15a077
nathen321 <walidjalaly43@gmail.com>
2023-11-28 07:48:16 -0800

    testing output

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,10 +19,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content != NULL)
-		write(fd, text_content, strlen(text_content));
-	else
-		return (-1);
-
+	{
+		if (write(fd, text_content, strlen(text_content)) == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
