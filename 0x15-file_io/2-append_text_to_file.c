0ae3b95aabcc11cf279a29f0dc430714aedb1e81
nathen321 <walidjalaly43@gmail.com>
2023-11-28 07:44:40 -0800

    testing output

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - ....
+ * @filename: ....
+ * @text_content: ....
+ *
+ * Return: Always 0.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+		write(fd, text_content, strlen(text_content));
+	else
+		return (-1);
+
+	close(fd);
+	return (1);
+}
