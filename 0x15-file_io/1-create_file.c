f89a97281c5f3c0f427e0a93cf635eb7a084b614
nathen321 <walidjalaly43@gmail.com>
2023-11-28 06:58:15 -0800

    testing output

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * read_textfile - ....
+ * create_file - ....
  * @filename: ....
- * @letters: ....
+ * @text_content: ....
  *
  * Return: Always 0.
  */
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC , 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (0);
