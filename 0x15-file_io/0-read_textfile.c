28100582d6b2beb73899fd0325c79244a527a840
nathen321 <walidjalaly43@gmail.com>
2023-11-28 03:50:12 -0800

    testing output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, readed;
 	char *buff = malloc(sizeof(char *) * letters);
 
-	if (!filename || !buff)
+	if (filename == NULL)
+		return (0);
+
+	if (buff == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY, 0600);
