819a1dbf46746148efbfe33f5bac0ccdb1ff5ccd
nathen321 <walidjalaly43@gmail.com>
2023-11-28 03:46:32 -0800

    testing output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,11 +2,29 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * main - check the code
+ * read_textfile - ....
+ * @filename: ....
+ * @letters: ....
  *
  * Return: Always 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd, readed;
+	char *buff = malloc(sizeof(char *) * letters);
 
+	if (!filename || !buff)
+		return (0);
+
+	fd = open(filename, O_RDONLY, 0600);
+
+	if (fd == -1)
+		return (0);
+
+	readed = read(fd, buff, letters);
+	write(1, buff, readed);
+
+	free(buff);
+	close(fd);
+	return (readed);
 }
