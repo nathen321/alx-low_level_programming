80a0de2bb63d9bb47fa1209d27ad2997b38a950d
nathen321 <walidjalaly43@gmail.com>
2023-11-28 07:06:03 -0800

    testing output

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,14 +11,14 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
-		return (0);
+		return (-1);
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 		write(fd, text_content, strlen(text_content));
 
 	close(fd);
