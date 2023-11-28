2d565ce82e2fe6f0ce5a7a821340b81e194da4a3
nathen321 <walidjalaly43@gmail.com>
2023-11-28 12:45:35 -0800

    testing output

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void copy(const char *file1,const char *file2);
+void copy_file(const char *src, const char *dest);
 
 #endif
