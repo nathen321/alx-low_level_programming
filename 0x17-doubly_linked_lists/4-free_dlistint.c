c80f7def1c0dafb03b1829005e3dfacea2781f9c
nathen321 <walidjalaly43@gmail.com>
2024-01-11 17:11:20 -0800

    ok

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - check the code
+ * @head: node
+ *
+ * Return: Always node.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *now = head, *next;
+
+	if (*head)
+	{
+		while (now != NULL)
+		{
+			next = now->next;
+			free(now);
+			now = next;
+		}
+	{
+}
