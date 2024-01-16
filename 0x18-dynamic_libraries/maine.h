683653c583b68db216411b4b4a63d61291737d84
nathen321 <walidjalaly43@gmail.com>
2024-01-16 01:40:08 -0800

    ok

--- 0x18-dynamic_libraries/maine.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAINE_H
-#define MAINE_H
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int div(int a, int b);
-int mod(int a, int b);
-#endif
--- 0x18-dynamic_libraries/mul.c
+++ 0x18-dynamic_libraries/mul.c
@@ -1,4 +1,3 @@
-#include "maine.h"
 #include <stdio.h>
 
 /**
