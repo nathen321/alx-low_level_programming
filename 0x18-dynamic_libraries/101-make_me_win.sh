91df29ac533cc4cfa32765a2b874e6a1fb4a4b4f
nathen321 <walidjalaly43@gmail.com>
2024-01-17 07:48:08 -0800

    ok

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P . https://raw.github.com/nathen321/alx-low_level_programming/master/0x18-dynamic_libraries/injecte.so
-export LD_PRELOAD="$PWD/injecte.so"
+wget -P .. https://raw.github.com/nathen321/alx-low_level_programming/master/0x18-dynamic_libraries/injecte.so
+export LD_PRELOAD="$PWD/../injecte.so"
