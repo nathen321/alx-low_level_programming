5ce46c1c9b9be771bc4e8c07b68ff806a7d9b2e3
nathen321 <walidjalaly43@gmail.com>
2024-02-04 15:39:42 -0800

    ok

--- 0x1A-hash_tables/4-main.c
+++ 0x1A-hash_tables/4-main.c
@@ -19,6 +19,9 @@ int main(void)
 	hash_table_set(ht, "Bob", "and Kris love asm");
 	hash_table_set(ht, "N", "queens");
 	hash_table_set(ht, "Asterix", "Obelix");
+	hash_table_set(ht, "Betty", "Cool");
+	hash_table_set(ht, "98", "Battery Street");
+	hash_table_set(ht, "c", "isfun");
 
 	value = hash_table_get(ht, "python");
 	printf("%s:%s\n", "python", value);
@@ -32,5 +35,9 @@ int main(void)
 	printf("%s:%s\n", "Betty", value);
 	value = hash_table_get(ht, "98");
 	printf("%s:%s\n", "98", value);
+	value = hash_table_get(ht, "c");
+	printf("%s:%s\n", "c", value);
+	value = hash_table_get(ht, "javascript");
+	printf("%s:%s\n", "javascript", value);
 	return (EXIT_SUCCESS);
 }
