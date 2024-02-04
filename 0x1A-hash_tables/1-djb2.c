1f4371bbee38485166c6b028909f9cec6af0e292
nathen321 <walidjalaly43@gmail.com>
2024-02-04 12:41:30 -0800

    ok

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,9 +1,9 @@
 /**
- *  * hash_djb2 - implementation of the djb2 algorithm
- *   * @str: string used to generate hash value
- *    *
- *     * Return: hash value
- *      */
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
