70b95e06d3027356b31c2b72f07b956dda5e7029
nathen321 <walidjalaly43@gmail.com>
2024-02-04 15:36:17 -0800

    ok

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx;
 	hash_node_t *node;
 
-	if (strcmp(key, "") == 0)
+	if (ht == NULL || key == NULL)
 		return (NULL);
 
 	idx = key_index((unsigned char *) key, ht->size);
