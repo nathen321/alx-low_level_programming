572bc160db0bf22a3853abb43628fa26b964a79b
nathen321 <walidjalaly43@gmail.com>
2024-02-04 15:25:39 -0800

    ok

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	hash_node_t node = NULL;
+
+	if (strcmp(key, "") == 0)
+		return (NULL);
+
+	idx = key_index((unsigned char *) key, ht->size);
+	node = ht->array[idx];
+	
+	if (node == NULL)
+		return (NULL);
+	
+	while (node && strcmp(key, node->key) != 0)
+		node = node->next;
+
+	return (node->value);
+}
